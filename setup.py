6885456957ce9a57581f8198cfca393103ac335c
Fede Calendino <federicocalendino@gmail.com>
2020-04-18 19:25:42 +0200

    Version 0.1a

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="wrap-genius",
-    version="0.1",
+    version="0.1a",
     url="https://github.com/federicocalendino/wrap-genius",
     license="MIT",
     description="python wrapper for api.genius.com",
