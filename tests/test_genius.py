d77ebf491f6d4dd6e3db1a3aa87717539ca782ec
Fede Calendino <federicocalendino@gmail.com>
2020-04-18 20:46:50 +0200

    Add more tests for genius

--- tests/test_genius.py
+++ tests/test_genius.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 import ddt
 
 from genius.api import Genius
+from genius.exceptions import APIException
 
 
 GENIUS_ACCESS_TOKEN = os.getenv("GENIUS_ACCESS_TOKEN")
@@ -25,6 +26,15 @@ class GeniusTest(TestCase):
         self.assertEqual(artist.name, artist_name)
 
     @ddt.data(
+        (860, "Gorillaz"),
+        (13585, "Daft Punk"),
+    )
+    @ddt.unpack
+    def test_search_artist(self, artist_id, artist_name):
+        artist = self.genius.search_artist(artist_name)
+        self.assertEqual(artist.id, artist_id)
+
+    @ddt.data(
         (3027437, "Saturnz Barz", "Gorillaz"),
         (3027418, "Ascension", "Gorillaz"),
         (53533, "On Melancholy Hill", "Gorillaz"),
@@ -39,3 +49,27 @@ class GeniusTest(TestCase):
         song = self.genius.get_song(song_id)
         self.assertEqual(song.title, song_title)
         self.assertEqual(song.primary_artist.name, artist_name)
+
+    @ddt.data(
+        (3027437, "Saturnz Barz", "Gorillaz", True),
+        (3027418, "Ascension", "Gorillaz", False),
+        (53533, "On Melancholy Hill", "Gorillaz", True),
+        (3027414, "Andromeda", "Gorillaz", False),
+        (151355, "Instant Crush", "Daft Punk", True),
+        (151349, "Giorgio by Moroder", "Daft Punk", False),
+        (151385, "Fragments of Time", "Daft Punk", True),
+        (72011, "Digital Love", "Daft Punk", False),
+    )
+    @ddt.unpack
+    def test_search_songs(self, song_id, song_title, artist_name, exact):
+        song = next(self.genius.search_songs(song_title, exact=exact))
+
+        self.assertEqual(song.id, song_id)
+        self.assertEqual(song.primary_artist.name, artist_name)
+
+    def test_api_exception(self):
+        with self.assertRaises(APIException):
+            self.genius.get_song(2678345123867)
+
+        with self.assertRaises(APIException):
+            self.genius.get_artist(2678345123867)
